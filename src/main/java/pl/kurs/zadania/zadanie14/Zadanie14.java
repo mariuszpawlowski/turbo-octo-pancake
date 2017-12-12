8461d1d2786a03e3ffc2107e013b9748e09c1d67
Your Name <you@example.com>
2017-12-12 09:59:53 +0100

    zad 14 - ciagle

--- src/main/java/pl/kurs/zadania/zadanie14/Zadanie14.java
+++ src/main/java/pl/kurs/zadania/zadanie14/Zadanie14.java
@@ -4,18 +4,25 @@ package pl.kurs.zadania.zadanie14;
 public class Zadanie14 {
 	
 	public static void main(String[] args) {
+		// gdzie tutaj korzystamy z metody toString?
+		
+	
+		
 		
 		Pozycja p1 = new Pozycja("Chleb", 1, 3.5);
-		System.out.println(p1);
+		System.out.println(p1); //w jaki sposób to jest wypisywane?
 		Pozycja p2 = new Pozycja("Cukier", 3, 4);
 		System.out.print(p2);
 		
-		Zamowienie z = new Zamowienie(20);
+		Zamowienie z = new Zamowienie(20); //co to jest 20?
 		z.dodajPozycje(p1);
 		z.dodajPozycje(p2);
-		System.out.println(z);
+		System.out.println(z); 
+		
+		
+	}
+		
 
 }
 
 	
-}
